3ed2526e21e13e6c9a41af979a5c516f1c1e20f4
esdrasgc <esdrasgcdev@gmail.com>
2024-04-06 18:49:46 -0300

    read all retornando lista

--- src/main/java/insper/store/instituicao/InstituicaoController.java
+++ src/main/java/insper/store/instituicao/InstituicaoController.java
@@ -1,12 +1,13 @@
 package insper.store.instituicao;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestHeader;
 
 
 @FeignClient(name = "store-instituicao")
@@ -23,7 +24,5 @@ public interface InstituicaoController {
     );
 
     @GetMapping("/instituicao")
-    public ResponseEntity<InstituicaoOut[]> readAll(
-        // @RequestHeader(required = false, name = "Authorization") String token
-    );
+    public ResponseEntity<List<InstituicaoOut>> readAll();
 }
